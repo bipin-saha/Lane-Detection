9e963b10eba050bcc9053b9dd56dfdfea28baf83
Bipin Saha <bipinsaha142@gmail.com>
2023-10-13 16:51:58 +0600

    canny edge detection

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import numpy as np
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-cv2.imshow('Result Image', gray)
+
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+canny = cv2.Canny(blur, 50, 100)
+cv2.imshow('Result Image', canny)
 cv2.waitKey(0)
