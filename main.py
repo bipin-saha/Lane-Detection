acdb5ad7dfbfd5cccd6d57a2129eb030d71bccec
Bipin Saha <bipinsaha142@gmail.com>
2023-10-13 17:41:26 +0600

    update hough line transform

--- main.py
+++ main.py
@@ -4,23 +4,38 @@ import matplotlib.pyplot as plt
 
 
 def canny_edge_detector(image):    
-    lane_image = np.copy(image)
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    #lane_image = np.copy(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     canny = cv2.Canny(blur, 50, 100)
     return canny
+
+def display_line(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 255), 2)
+    
+    return line_image
 
 def region_of_interest(image):
     height, width = image.shape
     polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image, np.uint8)
     cv2.fillPoly(mask, polygons, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 image = cv2.imread('test_image.jpg')
-
+lane_image = np.copy(image)
 
 canny = canny_edge_detector(image)
 roi = region_of_interest(canny)
-cv2.imshow("Result Image", roi)
+lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength = 100, maxLineGap = 5)
+line_image = display_line(roi, lines)
+
+#combination = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+
+cv2.imshow("Result Image", line_image)
 cv2.waitKey(0)
