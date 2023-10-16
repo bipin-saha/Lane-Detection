e2b4a39b197cfc5edc57604e84818044378ce07b
Bipin Saha <bipinsaha142@gmail.com>
2023-10-16 13:15:53 +0600

    latest update, work in videos

--- main.py
+++ main.py
@@ -16,25 +16,27 @@ def display_line(image, lines, lane_image):
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
             #cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 255), 2)
-            cv2.line(lane_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            #cv2.line(lane_image, )
+            cv2.line(lane_image, (x1, y1), (x2, y2), (255, 0, 0), 4)
     
     return lane_image
+
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
     y1 = image.shape[0]
     y2 = int((y1)*0.6)
     x1 = int((y1-intercept)/slope)
     x2 = int((y2-intercept)/slope)
+    
     return np.array([x1, y1, x2, y2])
 
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
+    
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        #print(parameters)
+    
         slope, intercept = parameters[0], parameters[1]
         if slope <0:
             left_fit.append((slope, intercept))
@@ -45,6 +47,7 @@ def average_slope_intercept(image, lines):
     right_fit_average = np.average(right_fit, axis=0)
     left_line = make_coordinates(image, left_fit_average)
     right_line = make_coordinates(image, right_fit_average)
+    
     return np.array([left_line, right_line])
 
 def region_of_interest(image):
@@ -53,20 +56,32 @@ def region_of_interest(image):
     mask = np.zeros_like(image, np.uint8)
     cv2.fillPoly(mask, polygons, 255)
     masked_image = cv2.bitwise_and(image, mask)
+    
     return masked_image
+
+
+"""
+#Detect Lane from Image
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
+"""
 
-canny = canny_edge_detector(image)
-roi = region_of_interest(canny)
-lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
+cap = cv2.VideoCapture("test2.mp4")
+while (cap.isOpened()):
+    _, frame = cap.read()
+    image = frame
+    lane_image = np.copy(image)
+    canny = canny_edge_detector(image)
+    roi = region_of_interest(canny)
+    lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
 
-avergaed_lines = average_slope_intercept(lane_image, lines)
+    avergaed_lines = average_slope_intercept(lane_image, lines)
+    line_image = display_line(roi, avergaed_lines, lane_image)
 
-line_image = display_line(roi, avergaed_lines, lane_image)
+    cv2.imshow("Result Image", line_image)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-#combination = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
-
-cv2.imshow("Result Image", line_image)
-cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows() 
