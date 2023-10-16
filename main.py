741d63a9d5b9e169babb7d7419c0257393730124
Bipin Saha <bipinsaha142@gmail.com>
2023-10-16 13:04:29 +0600

    averaged line output

--- main.py
+++ main.py
@@ -17,11 +17,16 @@ def display_line(image, lines, lane_image):
             x1, y1, x2, y2 = line.reshape(4)
             #cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 255), 2)
             cv2.line(lane_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            #cv2.line(lane_image, )
     
     return lane_image
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
-    pass
+    y1 = image.shape[0]
+    y2 = int((y1)*0.6)
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1, y1, x2, y2])
 
 def average_slope_intercept(image, lines):
     left_fit = []
@@ -40,6 +45,7 @@ def average_slope_intercept(image, lines):
     right_fit_average = np.average(right_fit, axis=0)
     left_line = make_coordinates(image, left_fit_average)
     right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
 
 def region_of_interest(image):
     height, width = image.shape
@@ -58,7 +64,7 @@ lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength = 40
 
 avergaed_lines = average_slope_intercept(lane_image, lines)
 
-line_image = display_line(roi, lines, lane_image)
+line_image = display_line(roi, avergaed_lines, lane_image)
 
 #combination = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
 
